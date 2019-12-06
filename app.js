95a1bd84291d3694444cd7dbc38fb95357751e5f
Aserikov <>
2019-12-06 19:13:17 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const mongoose = require('mongoose');
+
+const graphqlSchema = require('./graphql/schema/index');
+const graphqlResolvers = require('./graphql/resolvers/index');
+const isAuth = require('./middleware/is-auth');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(isAuth);
+
+app.use('/graphql', graphqlHttp({
+        schema: graphqlSchema,
+        rootValue: graphqlResolvers,
+        graphiql: true
+    })
+);
+
+mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${
+    process.env.MONGO_PASSWORD
+    }@cluster0-z41kv.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+).then(() => {
+    app.listen(3000);
+}).catch(err => {
+    console.log(err);
+})
